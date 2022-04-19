2beeb8663d60ea2bf6d89ddf0eededca37ce61ce
AU <99085476+AtsukiU@users.noreply.github.com>
2022-04-19 23:53:24 +0900

    commit

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -11,7 +11,7 @@ const Skills = () => {
   return (
     <Box py={10} w="100%" fontWeight={"bold"}>
       <SectionTitle>Skills</SectionTitle>
-      <Flex flexWrap={"wrap"} justifyContent="center">
+      <Flex minW="320" maxW="950" mx="auto" flexWrap={"wrap"}>
         <SkillCard
           SkillCardImage={HTMLCSSImage}
           SkillCardTitle={"HTML&CSS"}
